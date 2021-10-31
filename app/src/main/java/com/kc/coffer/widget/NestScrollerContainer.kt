12a52649ddbe24a52b56c67408f10661e8ecee68
coffer <zbq19930108@126.com>
2021-10-31 16:42:55 +0800

    code message : 解决滑动BUG

--- app/src/main/java/com/kc/coffer/widget/NestScrollerContainer.kt
+++ app/src/main/java/com/kc/coffer/widget/NestScrollerContainer.kt
@@ -1,7 +1,6 @@
 package com.kc.coffer.widget;
 
 import android.animation.ValueAnimator
-import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
@@ -44,12 +43,6 @@ open class NestScrollerContainer @JvmOverloads constructor(
      */
     private var isFirstRunAnim = false
 
-    init {
-        orientation = VERTICAL
-        headerView = View(context)
-        footerView = View(context)
-    }
-
     override fun onFinishInflate() {
         super.onFinishInflate()
         val layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, MAX_HEIGHT)
@@ -60,20 +53,23 @@ open class NestScrollerContainer @JvmOverloads constructor(
         scrollBy(0, MAX_HEIGHT)
     }
 
-    override fun onStartNestedScroll(child: View, target: View, axes: Int, type: Int): Boolean {
-        Log.i(
-            TAG,
-            "onStartNestedScroll  axes : $axes ,type : $type"
-        )
-        return target is RecyclerView
+    init {
+        orientation = VERTICAL
+        headerView = View(context)
+        footerView = View(context)
     }
 
+
+override fun onStartNestedScroll(child: View, target: View, axes: Int, type: Int): Boolean {
+    Log.i(TAG, "onStartNestedScroll  axes : $axes ,type : $type")
+    // 返回true表示处理事件
+    return target is RecyclerView
+}
+
     override fun onNestedScrollAccepted(child: View, target: View, axes: Int, type: Int) {
-        Log.i(
-            TAG,
-            "onNestedScrollAccepted  axes : $axes ,type : $type"
-        )
+        Log.i(TAG, "onNestedScrollAccepted  axes : $axes ,type : $type")
         if (animator == null) {
+            // 初始化动画对象
             animator = ReboundAnimator()
         }
     }
@@ -83,123 +79,38 @@ open class NestScrollerContainer @JvmOverloads constructor(
         Log.i(TAG, "onStopNestedScroll type : $type")
         // 服务初始位置
         isFirstRunAnim = false
-        if (scaleY != MAX_HEIGHT.toFloat()) {
+        if (scrollY != MAX_HEIGHT) {
             // 优化代码执行的效率
-            animator?.let {
-                it.startOfFloat(target, scaleY)
-            }
+            animator?.startOfFloat(target, scaleY)
         }
     }
 
-//    override fun onNestedScroll(
-//        target: View, dxConsumed: Int, dyConsumed: Int,
-//        dxUnconsumed: Int, dyUnconsumed: Int, type: Int
-//    ) {
-//        // 这个方法仅在目标View可以滚动时调用。
-//        Log.i(
-//            TAG, "onNestedScroll type : " + type + " ，dxConsumed ：" + dxConsumed + "，dyConsumed ："
-//                    + dyConsumed + "，dxUnconsumed：" + dxUnconsumed + "，dyUnconsumed：" + dyUnconsumed
-//        )
-//        parent.requestDisallowInterceptTouchEvent(true)
-//        // type == TYPE_NON_TOUCH 表示Filing，这个是NestedScrollingParent2对NestedScrollingParent的
-//        // 扩展优化，让RecycleView 在Filing 下更加顺滑
-//        if (type == ViewCompat.TYPE_NON_TOUCH) {
-//            //非手指触发的滑动，即Filing , 解决冗余fling问题
-//            if ((floor(scrollY.toDouble()) == 0.0 ||
-//                        ceil(scrollY.toDouble()) == (2 * MAX_HEIGHT).toDouble()) && !isFirstRunAnim
-//            ) {
-//                var startY = 0
-//                if (dyUnconsumed > 0) {
-//                    // 表示向上Filing，将尾视图滑动出来了
-//                    startY = 2 * MAX_HEIGHT
-//                }
-//                animator!!.startOfFloat(target, startY.toFloat())
-//                isFirstRunAnim = true
-//            }
-//            if (isFirstRunAnim) return
-//            // dy>0向下fling dy<0向上fling
-//            val showTop = dyUnconsumed < 0 && !target.canScrollVertically(-1)
-//            val showBottom = dyUnconsumed > 0 && !target.canScrollVertically(1)
-//            if (showTop || showBottom) {
-//                if (animator!!.isStarted) {
-//                    animator!!.pause()
-//                }
-//                scrollBy(0, damping(dyUnconsumed))
-//                if (animator!!.isPaused) {
-//                    //手动cancel 避免内存泄漏
-//                    animator!!.cancel()
-//                }
-//            }
-//            //调整错位
-//            adjust(dyUnconsumed, target)
-//        }
-//    }
-
-//    override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
-//        Log.i(TAG, "onNestedPreScroll type : $type")
-//        // 如果在自定义ViewGroup之上还有父View交给我来处理
-//        parent.requestDisallowInterceptTouchEvent(true)
-//        if (type == ViewCompat.TYPE_TOUCH) {
-//            Log.i(
-//                TAG,
-//                "onNestedPreScroll dy : $dy , getScrollY : $scrollY"
-//            )
-//            //手指触发的滑动 dy < 0向下scroll dy > 0向上scroll
-//            // canScrollVertically ：检测目标视图（RecycleView）是否可以在特定方向垂直滚动，负数表示向上滚
-//            // 当前的滚动如果是向下，并且子View已经不能向上滚动时（表示滑到顶了），表示要显示顶部，否则向下
-//            // 这里需要注意下，在初始化时，为了隐藏头视图，整个View已经向上滚动了MAX_HEIGHT（200），因此此时
-//            // 向上滚动时，getScrollY == MAX_HEIGHT（200)，在RecycleView自己的内容可以滚动时，getScrollY
-//            // 还是MAX_HEIGHT（200)，因为仅头视图在外面，当滚到底，即RecycleView滚上去显示尾视图时，
-//            // getScrollY == 头视图的高度 + 尾视图的高度
-//            val hiddenTop = dy > 0 && scrollY < MAX_HEIGHT && !target.canScrollVertically(-1)
-//            val showTop = dy < 0 && !target.canScrollVertically(-1)
-//            val hiddenBottom = dy < 0 && scrollY > MAX_HEIGHT && !target.canScrollVertically(1)
-//            val showBottom = dy > 0 && !target.canScrollVertically(1)
-//            animator?.let {
-//                if (hiddenTop || showTop || hiddenBottom || showBottom) {
-//                    if (it.isStarted) {
-//                        animator!!.pause()
-//                    }
-//                    scrollBy(0, damping(dy))
-//                    if (it.isPaused) {
-//                        //手动cancel 避免内存泄漏
-//                        it.cancel()
-//                    }
-//                    // 事件交给父View消耗，consumed[1]表示父View的y轴
-//                    consumed[1] = dy
-//                }
-//            }
-//            //调整错位
-//            adjust(dy, target)
-//        }
-//    }
-
     override fun onNestedScroll(
-        target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int,
-        dyUnconsumed: Int, type: Int
+        target: View, dxConsumed: Int, dyConsumed: Int,
+        dxUnconsumed: Int, dyUnconsumed: Int, type: Int
     ) {
         // 这个方法仅在目标View可以滚动时调用。
         Log.i(
-            TAG, "onNestedScroll type : " + type + " ，dxConsumed ：" + dxConsumed + "，dyConsumed ："
+            NestScrollerContainer.TAG, "onNestedScroll type : " + type + " ，dxConsumed ：" + dxConsumed + "，dyConsumed ："
                     + dyConsumed + "，dxUnconsumed：" + dxUnconsumed + "，dyUnconsumed：" + dyUnconsumed
         )
         parent.requestDisallowInterceptTouchEvent(true)
         // type == TYPE_NON_TOUCH 表示Filing，这个是NestedScrollingParent2对NestedScrollingParent的
         // 扩展优化，让RecycleView 在Filing 下更加顺滑
         if (type == ViewCompat.TYPE_NON_TOUCH) {
-            //非手指触发的滑动，即Filing
-            //解决冗余fling问题
-            if ((Math.floor(scrollY.toDouble()) == 0.0 || Math.ceil(scrollY.toDouble()) == (2 * MAX_HEIGHT).toDouble()) && !isFirstRunAnim) {
+            //非手指触发的滑动，即Filing , 解决冗余fling问题
+            if ((floor(scrollY.toDouble()) == 0.0 ||
+                        ceil(scrollY.toDouble()) == (2 * NestScrollerContainer.MAX_HEIGHT).toDouble()) && !isFirstRunAnim
+            ) {
                 var startY = 0
                 if (dyUnconsumed > 0) {
                     // 表示向上Filing，将尾视图滑动出来了
-                    startY = 2 * MAX_HEIGHT
+                    startY = 2 * NestScrollerContainer.MAX_HEIGHT
                 }
                 animator!!.startOfFloat(target, startY.toFloat())
                 isFirstRunAnim = true
             }
             if (isFirstRunAnim) return
-
             // dy>0向下fling dy<0向上fling
             val showTop = dyUnconsumed < 0 && !target.canScrollVertically(-1)
             val showBottom = dyUnconsumed > 0 && !target.canScrollVertically(1)
@@ -219,11 +130,14 @@ open class NestScrollerContainer @JvmOverloads constructor(
     }
 
     override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
-        Log.i(TAG, "onNestedPreScroll type : $type")
+        Log.i(NestScrollerContainer.TAG, "onNestedPreScroll type : $type")
         // 如果在自定义ViewGroup之上还有父View交给我来处理
         parent.requestDisallowInterceptTouchEvent(true)
         if (type == ViewCompat.TYPE_TOUCH) {
-            Log.i(TAG, "onNestedPreScroll dy : $dy , getScrollY : $scrollY")
+            Log.i(
+                NestScrollerContainer.TAG,
+                "onNestedPreScroll dy : $dy , getScrollY : $scrollY"
+            )
             //手指触发的滑动 dy < 0向下scroll dy > 0向上scroll
             // canScrollVertically ：检测目标视图（RecycleView）是否可以在特定方向垂直滚动，负数表示向上滚
             // 当前的滚动如果是向下，并且子View已经不能向上滚动时（表示滑到顶了），表示要显示顶部，否则向下
@@ -231,21 +145,23 @@ open class NestScrollerContainer @JvmOverloads constructor(
             // 向上滚动时，getScrollY == MAX_HEIGHT（200)，在RecycleView自己的内容可以滚动时，getScrollY
             // 还是MAX_HEIGHT（200)，因为仅头视图在外面，当滚到底，即RecycleView滚上去显示尾视图时，
             // getScrollY == 头视图的高度 + 尾视图的高度
-            val hiddenTop = dy > 0 && scrollY < MAX_HEIGHT && !target.canScrollVertically(-1)
+            val hiddenTop = dy > 0 && scrollY < NestScrollerContainer.MAX_HEIGHT && !target.canScrollVertically(-1)
             val showTop = dy < 0 && !target.canScrollVertically(-1)
-            val hiddenBottom = dy < 0 && scrollY > MAX_HEIGHT && !target.canScrollVertically(1)
+            val hiddenBottom = dy < 0 && scrollY > NestScrollerContainer.MAX_HEIGHT && !target.canScrollVertically(1)
             val showBottom = dy > 0 && !target.canScrollVertically(1)
-            if (hiddenTop || showTop || hiddenBottom || showBottom) {
-                if (animator!!.isStarted) {
-                    animator!!.pause()
+            animator?.let {
+                if (hiddenTop || showTop || hiddenBottom || showBottom) {
+                    if (it.isStarted) {
+                        animator!!.pause()
+                    }
+                    scrollBy(0, damping(dy))
+                    if (it.isPaused) {
+                        //手动cancel 避免内存泄漏
+                        it.cancel()
+                    }
+                    // 事件交给父View消耗，consumed[1]表示父View的y轴
+                    consumed[1] = dy
                 }
-                scrollBy(0, damping(dy))
-                if (animator!!.isPaused) {
-                    //手动cancel 避免内存泄漏
-                    animator!!.cancel()
-                }
-                // 事件交给父View消耗，consumed[1]表示父View的y轴
-                consumed[1] = dy
             }
             //调整错位
             adjust(dy, target)
@@ -299,17 +215,16 @@ open class NestScrollerContainer @JvmOverloads constructor(
     /**
      * 回弹动画
      */
-    @SuppressLint("Recycle")
     private inner class ReboundAnimator : ValueAnimator() {
         private var target: View? = null
-
-        init {
-            interpolator = DecelerateInterpolator()
-            duration = 260
+        private fun init() {
+            this.interpolator = DecelerateInterpolator() //添加减速插值器
+            this.duration = 260
             //添加值更新监听器
             addUpdateListener {
                 val currValue = animatedValue as Float
                 scrollTo(0, currValue.toInt())
+                // 调整错位问题(强转精度损失造成的错位)
                 if (scrollY > MAX_HEIGHT && !target!!.canScrollVertically(-1)) {
                     scrollTo(0, MAX_HEIGHT)
                 }
@@ -320,11 +235,13 @@ open class NestScrollerContainer @JvmOverloads constructor(
         }
 
         fun startOfFloat(target: View, startY: Float) {
-            this.setFloatValues(startY, MAX_HEIGHT.toFloat())
+            setFloatValues(startY, MAX_HEIGHT.toFloat())
             this.target = target
-            this.start()
+            start()
+        }
+
+        init {
+            init()
         }
     }
-
-
 }
